76d5f74592c06fa65047706c81ad9ed59feb40e2
wktk <mail@wktk.jp>
2013-07-12 09:12:03 +0900

    Add spec for #verifiy_credentials

---
+++ spec/croudia/api/account_spec.rb
@@ -0,0 +1,21 @@
+require 'helper'
+
+describe Croudia::API::Account do
+  before do
+    @client = Croudia::Client.new
+  end
+
+  describe '#verify_credentials' do
+    before do
+      stub_get('/account/verify_credentials.json').to_return(
+        body: fixture(:user),
+        header: { content_type: 'application/json; charset=utf-8' }
+      )
+    end
+
+    it 'requests the correct resource' do
+      @client.verify_credentials
+      expect(a_get('/account/verify_credentials.json')).to have_been_made
+    end
+  end
+end
