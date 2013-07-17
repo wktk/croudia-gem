98f8b3f18ef543f40ce8d4cfea636a1af4824ac2
wktk <mail@wktk.jp>
2013-07-17 22:28:25 +0900

    Delete play_* attrs of Status object
    It was a bug in the document.
    See: https://croudia.com/voices/show/1268070

--- lib/croudia/status.rb
+++ lib/croudia/status.rb
@@ -10,17 +10,14 @@ module Croudia
       :in_reply_to_screen_name, :in_reply_to_status_id_str,
       :in_reply_to_status_id, :in_reply_to_user_id_str,
       :in_reply_to_user_id, :spread, :spread_count, :spread_status,
-      :play_spread, :play_spread_status, :reply_status, :source,
-      :text, :user
+      :reply_status, :source, :text, :user
 
     def initialize(attrs={})
       user = attrs.delete('user')
-      pss = attrs.delete('play_spread_status')
       reply_status = attrs.delete('reply_status')
       spread_status = attrs.delete('spread_status')
       super(attrs)
       @attrs['user'] = Croudia::User.new(user) if user
-      @attrs['play_spread_status'] = Croudia::Status.new(pss) if pss
       @attrs['reply_status'] = Croudia::Status.new(reply_status) if reply_status
       @attrs['spread_status'] = Croudia::Status.new(spread_status) if spread_status
     end
