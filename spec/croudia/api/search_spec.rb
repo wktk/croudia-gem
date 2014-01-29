498e036aa231fee76ae07c0b53ef60ad69652b58
wktk <mail@wktk.jp>
2014-01-29 09:21:42 +0900

    Add specs for the new methods
    - #search_user_by_profile
    - #search_favorites

--- spec/croudia/api/search_spec.rb
+++ spec/croudia/api/search_spec.rb
@@ -51,4 +51,51 @@ describe Croudia::API::Search do
       subject.each { |user| expect(user).to be_a Croudia::User }
     end
   end
+
+  describe '#search_user_by_profile' do
+    before do
+      stub_get('/profile/search.json').with(query: {
+        q: 'hoge',
+      }).to_return(
+        body: fixture(:users),
+        headers: { content_type: 'application/json; charset=utf-8' }
+      )
+    end
+
+    it 'requests the correct resource' do
+      @client.search_user_by_profile('hoge')
+      expect(a_get('/profile/search.json').with(query: {
+        q: 'hoge',
+      })).to have_been_made
+    end
+
+    it 'returns an array of Croudia::User' do
+      subject = @client.search_user_by_profile('hoge')
+      expect(subject).to be_a Array
+      subject.each { |user| expect(user).to be_a Croudia::User }
+    end
+  end
+
+  describe '#search_favorites' do
+    before do
+      stub_get('/search/favorites.json').with(query: {
+        q: 'hoge',
+      }).to_return(
+        body: fixture(:search_results),
+        headers: { content_type: 'application/json; charset=utf-8' }
+      )
+    end
+
+    it 'requests the correct resource' do
+      @client.search_favorites('hoge')
+      expect(a_get('/search/favorites.json').with(query: {
+        q: 'hoge',
+      })).to have_been_made
+    end
+
+    it 'returns Croudia::SearchResults' do
+      subject = @client.search_favorites('hoge')
+      expect(subject).to be_a Croudia::SearchResults
+    end
+  end
 end
